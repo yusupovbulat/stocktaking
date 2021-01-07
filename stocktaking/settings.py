4aef0a64d34be163189ef2ce0f00210889465181
Bulat Yusupov <yusupovbulat@gmail.com>
2021-01-07 22:21:39 +0500

    Replaced secret key reference to environ variable

--- stocktaking/settings.py
+++ stocktaking/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mm+8oy8dm89tko$4@y#j=!nl8caf1yw5jc-327k2&^_bjw!&-1'
+SECRET_KEY = env(‘SECRET_KEY’)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
