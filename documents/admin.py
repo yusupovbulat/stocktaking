c8b76ee8a8894512abe8837640209d14931078bc
Bulat Yusupov <yusupovbulat@gmail.com>
2021-01-09 20:15:19 +0500

    Registered models in admin panel

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import BaseDocument, Document, DocumentName, Provider
+
 # Register your models here.
+admin.site.register(Document)
+admin.site.register(DocumentName)
+admin.site.register(Provider)
+admin.site.register(BaseDocument)
